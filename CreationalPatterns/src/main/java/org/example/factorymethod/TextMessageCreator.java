c2ea6998907ade2b28602843997db4f916646340
Prajwal-S-6 <prajwals12021999@gmail.com>
2025-03-28 16:11:46 +0530

    Factory method pattern

--- CreationalPatterns/src/main/java/org/example/factorymethod/TextMessageCreator.java
+++ CreationalPatterns/src/main/java/org/example/factorymethod/TextMessageCreator.java
@@ -6,6 +6,8 @@ import org.example.factorymethod.message.TextMessage;
 
 /**
  * Provides implementation for creating Text messages
+ * Subclass will provide the implementation for objects
+ * if new type is added then new subclass creator will create objects for it
  */
 public class TextMessageCreator extends MessageCreator {
 
