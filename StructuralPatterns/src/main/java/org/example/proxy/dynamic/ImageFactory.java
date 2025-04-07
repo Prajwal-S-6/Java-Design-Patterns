ea51060fbaa6ada3bea4e58a40a10feec091df2e
Prajwal-S-6 <prajwals12021999@gmail.com>
2025-04-07 18:50:55 +0530

    Proxy Design pattern

--- StructuralPatterns/src/main/java/org/example/proxy/dynamic/ImageFactory.java
+++ StructuralPatterns/src/main/java/org/example/proxy/dynamic/ImageFactory.java
@@ -6,9 +6,9 @@ import java.lang.reflect.Proxy;
 
 //Factory to get image objects.
 public class ImageFactory {
-	//We'll provide proxy to caller instead of real object
+    //We'll provide proxy to caller instead of real object
     public static Image getImage(String name) {
-        return (Image)Proxy.newProxyInstance(ImageFactory.class.getClassLoader(), new Class[]{Image.class},
+        return (Image) Proxy.newProxyInstance(ImageFactory.class.getClassLoader(), new Class[]{Image.class},
                 new ImageInvocationHandler(name));
     }
 }
